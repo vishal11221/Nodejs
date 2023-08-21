ce2db0cfcc60f11977ab17c55c556f4feb989252
vishal <vishal.raj@reconnectenergy.com>
2023-08-21 11:18:18 +0530

    Simple-Api

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = [ 
+    {
+    name:'vishal raj',
+    email:'itsvishalraj98@gmail.com',
+    Address:'Motihari',
+    Phone: 8809739667
+    },
+   {
+    name: 'rohit kumar',
+    email: 'rohit@11gmail.com',
+    Address: 'Bettiah',
+    phone: 99087564040
+   },
+   {
+    name: 'vinay kumar',
+    email: 'vinay11@gmail.com',
+    Address: 'Muzaffarpur',
+    phone: 9199605544
+   }
+]
+
+module.exports = data;
